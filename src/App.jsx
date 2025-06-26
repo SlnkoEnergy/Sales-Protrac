90603942a9b5f91e0e5448855914b84bfe40f111
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-06-26 11:32:49 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "@/modules/dashboard/pages/Dashboard";
 import Leads from "@/modules/leads/pages/leads";
 import LoginPage from "@/modules/auth/pages/Login_Page";
-import LeadProfile from "@/modules/leads/pages/LeadProfile";
+import LeadProfile from "@/modules/leads/pages/leadProfile";
 import Tasks from "@/modules/task/pages/TaskDashboard"
 import AddTask from "@/components/task/AddTask"
 import ViewTask from "@/components/task/ViewTask"
